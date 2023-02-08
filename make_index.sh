f11882c5ac0cca86dd75a598c7316e75ea6f8374
audrey-onfroy <audrey.onfroy@inserm.fr>
2023-02-08 18:22:49 +0100

    Documentation

--- make_index.sh
+++ make_index.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+#====================== GET OPTIONS
+
+#-------- Get option
+ROOT="/home/aurelien/Documents/Audrey/git_analysis"
+MENU="/home/aurelien/Documents/Audrey/git_book/index_builder/site-index.html"
+IGNORE="/libs/|/pipeline_info/|/SCENIC/|index.html"
 OUTPUT="/home/aurelien/Documents/Audrey/git_book/index.html"
 
-# Generate menu
-menu_file="/home/aurelien/Documents/Audrey/git_book/index_builder/site-index.html"
+while getopts r:m:i:o: flag
+do
+    case "${flag}" in
+        r) ROOT=${OPTARG};;
+        m) MENU=${OPTARG};;
+        i) IGNORE=${OPTARG};;
+        o) OUTPUT=${OPTARG};;
+    esac
+done
 
-rm -f $menu_file
+#====================== BUILD INDEX PAGE
 
 /home/aurelien/Documents/Audrey/git_book/index_builder/make_tree.sh \
--r /home/aurelien/Documents/Audrey/git_analysis \
--o $menu_file \
--i "/libs/|/pipeline_info/|/SCENIC/"
+-r $ROOT \
+-o $MENU \
+-i $IGNORE
 
 # Generate index
-cat ./index_builder/index_top.html > $OUTPUT
+cat /home/aurelien/Documents/Audrey/git_book/index_builder/index_top.html > $OUTPUT
 sed -i -e '$a\' $OUTPUT
-cat $menu_file >> $OUTPUT
+cat $MENU >> $OUTPUT
 sed -i -e '$a\' $OUTPUT
-cat ./index_builder/index_bottom.html >> $OUTPUT
+cat /home/aurelien/Documents/Audrey/git_book/index_builder/index_bottom.html >> $OUTPUT
